1effcc97feea87fac5b9e786e03072582bf84142
Abbas Sarwar <abbassarwar100@gmail.com>
2023-06-24 15:12:43 +0500

    Fixed linters

--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,8 @@ require_relative 'person'
 
 class Teacher < Person
   attr_reader :specialization
-  def initialize(age, specialization, name= "unknown", parent_permission: true)
+
+  def initialize(age, specialization, name = 'unknown', parent_permission: true)
     @specialization = specialization
     super(age, parent_permission, name)
   end
